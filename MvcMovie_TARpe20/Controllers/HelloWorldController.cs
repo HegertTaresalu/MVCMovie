13f5844fe3644ac8277bd666fed266e4eaf8255d
opilane <opilane@HP-CZC6297B8B>
2021-10-14 10:01:07 +0300

    add controller

---
+++ MvcMovie_TARpe20/Controllers/HelloWorldController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace MvcMovie_TARpe20.Controllers
+{
+
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numTimes=1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, Numtimes is {numTimes} ");
+        }
+
+        public string Hommik()
+        {
+
+            return "hommik";
+
+        }
+
+
+    }
+}
